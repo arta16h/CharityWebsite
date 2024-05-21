e02b64de55c19a1582ca9a5d03772a4f36c600a8
arta16h <sevda.hayati2015@gmail.com>
2024-05-21 22:44:36 +0330

    Chore: home view - send email in contact us

--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+
+from Charity.settings import mail
 from .forms import ContactUsForm, VolunteerRegisterForm
 # Create your views here.
 
@@ -6,12 +9,16 @@ def home(request) :
     if request.method == "POST":
         form = ContactUsForm(request.POST or None)
         if form.is_valid():
-            data = form.save(commit=False)
-            data.user = request.user
-            data.save()
-            return redirect('home')
-    form = ContactUsForm()
-    context = {'form' : form}
+            cd = form.cleaned_data
+            name = cd['name']
+            email = cd['email']
+            subject = cd['subject']
+            content = cd['content']
+            message = "نام:{0}\n ایمیل:{1}\n پیام:{3}".format(name, email, content)
+            send_mail(subject, message, mail, ['sevda.hayati2015@gmail.com'] ,fail_silently=False)
+    else :
+        form = ContactUsForm()
+        context = {'form' : form}
     return render(request, "home.html", context)
 
 
