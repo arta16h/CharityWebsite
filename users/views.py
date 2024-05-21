01d88f67e5f87af488354786e70a1cb81544232b
arta16h <sevda.hayati2015@gmail.com>
2024-05-21 20:34:45 +0330

    Chore: home view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import ContactUsForm
 # Create your views here.
+
+def home(request) :
+    if request.method == "POST":
+        form = ContactUsForm(request.POST or None)
+        if form.is_valid():
+            data = form.save(commit=False)
+            data.user = request.user
+            data.save()
+            return redirect('home')
+    form = ContactUsForm()
+    context = {'form' : form}
+    return render(request, "home.html", context)
