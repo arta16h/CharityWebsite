ab543e09723f46fcf48fa1d712de157db16c2f31
arta16h <sevda.hayati2015@gmail.com>
2024-05-21 21:14:44 +0330

    Chore: volunteer register view

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import ContactUsForm
+from .forms import ContactUsForm, VolunteerRegisterForm
 # Create your views here.
 
 def home(request) :
@@ -13,3 +13,15 @@ def home(request) :
     form = ContactUsForm()
     context = {'form' : form}
     return render(request, "home.html", context)
+
+
+def volunteer_register(request):
+    if request.method == "POST":
+        form = VolunteerRegisterForm(request.POST or None)
+        if form.is_valid():
+            data = form.save(commit=False)
+            data.user = request.user
+            data.save()
+            return redirect('users:volunteer')
+    form = VolunteerRegisterForm()
+    return render(request, "users/volunteer.html", {"form": form})
