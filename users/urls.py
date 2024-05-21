d146885a6ae0269281c6a0b71b74bff8be1ad3c2
arta16h <sevda.hayati2015@gmail.com>
2024-05-21 21:15:45 +0330

    Chore: volunteer url

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, volunteer_register
 
 urlpatterns = [
     path("home/", home, name="home"),
+    path("volunteer/", volunteer_register, name="volunteer"),
 ]
