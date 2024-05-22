a9591f5ff874cdcfc6b228b62e8d7a4124da73ef
arta16h <sevda.hayati2015@gmail.com>
2024-05-22 05:03:47 +0330

    Chore: about us url

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, volunteer_register
+from .views import home, volunteer_register, about_us
 
 urlpatterns = [
     path("home/", home, name="home"),
     path("volunteer/", volunteer_register, name="volunteer"),
+    path("abou-us/", about_us, name='about-us'),
 ]
