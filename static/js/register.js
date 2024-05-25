f02abeac5893d46c872390d2aa9229f521ec4c1b
arta16h <sevda.hayati2015@gmail.com>
2024-05-26 03:19:22 +0330

    Chore: showing checking message for phone

--- static/js/register.js
+++ static/js/register.js
@@ -1,7 +1,12 @@
 const phonefield = document.querySelector("#phonefield");
-const feedBackArea = document.querySelector('.invalid_feedback')
+const feedBackArea = document.querySelector('.invalid_feedback');
+const PhoneSuccessOutput = document.querySelector(".PhoneSuccessOutput");
+
+
 phonefield.addEventListener('keyup', (e) => {
     const PhoneVal = e.target.value;
+    PhoneSuccessOutput.style.display = "block";
+    PhoneSuccessOutput.textContent = 'در حال بررسی'
 
     phonefield.classList.remove("is-invalid");
     feedBackArea.style.display="none";
@@ -14,6 +19,7 @@ phonefield.addEventListener('keyup', (e) => {
         .then((res) => res.json())
         .then((data) => {
             console.log("data", data);
+            PhoneSuccessOutput.style.display = "none";
             if (data.PhoneError){
                 phonefield.classList.add("is-invalid");
                 feedBackArea.style.display="block";
