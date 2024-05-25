32ea35567a770cf57c88f676b432b7ba1b0144b0
arta16h <sevda.hayati2015@gmail.com>
2024-05-26 02:52:28 +0330

    Fix: about url

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,5 @@ from .views import home, volunteer_register, about_us
 urlpatterns = [
     path("home/", home, name="home"),
     path("volunteer/", volunteer_register, name="volunteer"),
-    path("abou-us/", about_us, name='about-us'),
+    path("about/", about_us, name='about'),
 ]
