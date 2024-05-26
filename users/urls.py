a4fe236e912cb0cfeece8e44d2b95c33775ae52e
arta16h <sevda.hayati2015@gmail.com>
2024-05-26 17:50:10 +0330

    Chore: login url

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home, volunteer_register, about_us, RegistrationView
+from .views import (home, volunteer_register, about_us, RegistrationView, 
+                    LoginView)
 
 urlpatterns = [
     path("home/", home, name="home"),
     path("volunteer/", volunteer_register, name="volunteer"),
     path("about/", about_us, name='about'),
     path("register/", RegistrationView.as_view(), name='register'),
+    path("login/", LoginView.as_view(), name='login'),
 ]
