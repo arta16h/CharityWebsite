eaf93d47f2a036683968cccc564f23108c490c38
arta16h <sevda.hayati2015@gmail.com>
2024-05-26 05:14:27 +0330

    Chore: Registration url

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, volunteer_register, about_us
+from .views import home, volunteer_register, about_us, RegistrationView
 
 urlpatterns = [
     path("home/", home, name="home"),
     path("volunteer/", volunteer_register, name="volunteer"),
     path("about/", about_us, name='about'),
+    path("register/", RegistrationView.as_view(), name='register'),
 ]
