4ac38d20fe9ae5b51a677c9faf62cfc4f72802e8
arta16h <sevda.hayati2015@gmail.com>
2024-06-06 15:11:27 +0330

    Chore: blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model) :
+    title = models.CharField(max_length = 255)
+    author = models.CharField(max_length = 100)
+    content = models.TextField()
+    category = models.ManyToManyField(Category)
+    keywords = models.TextField(null=True, blank=True)
+    imageUrl = models.URLField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id}, {self.title}"
