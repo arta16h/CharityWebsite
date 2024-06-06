5b0a95bdfb3f60c0fcdbcd0723a85c4ca458de90
arta16h <sevda.hayati2015@gmail.com>
2024-06-06 22:45:00 +0330

    Chore: BlogUpdateView url

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
-from .views import BlogListView, BlogDetailView, BlogCreateView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
 from django.urls import path
 
 urlpatterns =[
     path('blogs/', BlogListView.as_view(), name='blog-list'),
     path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog-detail'),
     path('blog/new/', BlogCreateView.as_view(), name='new-blog'),
+    path('blog/<int:pk>/update/', BlogUpdateView.as_view(), name='blog-update'),
 ]
