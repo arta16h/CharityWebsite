2249c5c4dd6fce343a9348a64165c4837dcbe80f
arta16h <sevda.hayati2015@gmail.com>
2024-06-06 23:00:41 +0330

    Chore: BlogDeleteView url

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 from django.urls import path
 
 urlpatterns =[
@@ -6,4 +6,5 @@ urlpatterns =[
     path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog-detail'),
     path('blog/new/', BlogCreateView.as_view(), name='new-blog'),
     path('blog/<int:pk>/update/', BlogUpdateView.as_view(), name='blog-update'),
+    path('blog/<int:pk>/delete/', BlogDeleteView.as_view(), name='blog-delete'),
 ]
