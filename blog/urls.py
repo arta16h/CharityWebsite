02fe11cff8ed3b619a8664eb122d4ea6df0f0d83
arta16h <sevda.hayati2015@gmail.com>
2024-06-06 16:34:41 +0330

    Chore: blog create url

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView
 from django.urls import path
 
 urlpatterns =[
     path('blogs/', BlogListView.as_view(), name='blog-list'),
     path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog-detail'),
+    path('blog/new/', BlogCreateView.as_view(), name='new-blog'),
 ]
