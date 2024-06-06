1e41d84e1fa4ae6e283913c791604c8e55d28086
arta16h <sevda.hayati2015@gmail.com>
2024-06-06 15:12:04 +0330

    Chore: category model

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
 class Blog(models.Model) :
     title = models.CharField(max_length = 255)
     author = models.CharField(max_length = 100)
