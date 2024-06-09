4c5c9d033c3e9885a80b1c45d0fd41e0882b3bd5
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2024-06-10 01:39:35 +0330

    chore: volunteer model changes

--- users/models.py
+++ users/models.py
@@ -6,57 +6,6 @@ from django.utils.translation import gettext_lazy as _
 from .enums import *
 # Create your models here.
 
-GENDER_CHOICES = (
-    (1, 'مونث'),
-    (2, 'مذکر'),
-)
-
-ABILITIES_CHOICES = (
-    ("piping", "لوله کشی"),
-    ("building", "بنا"),
-    ("welder", "جوشکار"),
-    ("electrician", "برقکار"),
-    ("plaster", "گچ کار"),
-    ("facilities", "تاسیسات"),
-    ("graphist", "گرافیست"),
-    ("programmer", "برنامه نویس"),
-    ("photographer", "عکاس"),
-    ("Cameraman", "فیلم بردار"),
-    ("editor", "ویرایشگر"),
-    ("content", "تولید محتوا"),
-    ("speaker", "گوینده"),
-    ("kindergarten", "مهدکودک"),
-    ("translator", "مترجم عربی"),
-    ("PA", "پزشک عمومی"),
-    ("specialist", "پزشک متخصص"),
-    ("psychologist", "روانشناس"),
-    ("consult", "مشاوره"),
-    ("dentist", "دندان پزشک"),
-    ("pharmacist", "داروساز"),
-    ("nurse", "پرستار"),
-    ("executive", "پشتیبان اجرایی"),
-    ("cook", "آشپز"),
-    ("heavy vehicle", "راننده ماشین سنگین"),
-    ("pickup truck", "راننده وانت"),
-)
-
-MARITAL_CHOICES = (
-    (1, 'مجرد'),
-    (2, 'متاهل'),
-)
-
-EDU_CHOICES = (
-    (1, 'دیپلم'),
-    (2, 'کاردانی'),
-    (3, 'کارشناسی'),
-    (4, 'کارشناسی ارشد'),
-    (5, 'دکتری'),
-)
-
-EXP_CHOICES = (
-    (1, 'دارم'),
-    (2, 'ندارم'),
-)
 
 _REGEX = r'09(\d{9})$'
 phone_validator = RegexValidator(_REGEX)
