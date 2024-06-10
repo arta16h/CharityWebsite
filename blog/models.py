79a22e6fff808a17442d2cbc60f5a230e2f37180
arta16h <sevda.hayati2015@gmail.com>
2024-06-10 10:06:22 +0330

    Fix: auth user model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
 
@@ -12,7 +12,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
-    
+User = settings.AUTH_USER_MODEL    
 class Blog(models.Model) :
     title = models.CharField(max_length = 255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
