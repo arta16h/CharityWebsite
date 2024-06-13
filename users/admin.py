e7ad7afca2254b38f716fde0d8bcc6f7cb961bef
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2024-06-14 00:01:45 +0330

    chore: register user in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Job, Volunteer
+from .models import Job, Volunteer, User
 
 # Register your models here.
 
 admin.site.register(Job)
 admin.site.register(Volunteer)
+admin.site.register(User)
