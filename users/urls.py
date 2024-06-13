7362477f1c2a9832f1c4df25340eda284aa2470a
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2024-06-14 00:06:39 +0330

    chore: add send_otp, verify_otp, and validate_phone view to urls

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (home, volunteer_register, about_us, RegistrationView, 
-                    LoginView, LogoutView, dashboard)
+                    LoginView, LogoutView, SendOtpView, VerifyOtpView, PhoneValidationView, dashboard)
 
 urlpatterns = [
     path("home/", home, name="home"),
@@ -9,5 +9,8 @@ urlpatterns = [
     path("register/", RegistrationView.as_view(), name='register'),
     path("login/", LoginView.as_view(), name='login'),
     path("logout/", LogoutView.as_view(), name='logout'),
+    path("send_otp/", SendOtpView.as_view() , name="send_otp"),
+    path("verify_otp/", VerifyOtpView.as_view(), name="verify_otp"),
+    path("validate_phone/", PhoneValidationView.as_view(), name="validate_phone"),
     path("dashboard/", dashboard, name="dashboard"),
 ]
