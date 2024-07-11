63c851c3279f763bf3b48a69cd6b8902bd681946
arta16h <sevda.hayati2015@gmail.com>
2024-07-11 14:14:01 +0330

    Chore: django-jalali-date in volunteer form

--- users/forms.py
+++ users/forms.py
@@ -3,10 +3,8 @@ from django.urls import reverse_lazy
 from django.core.validators import RegexValidator
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django_jalali.forms import jDateInput
-from django_jalali.admin.widgets import AdminjDateWidget
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from jalali_date.fields import JalaliDateField
+from jalali_date.widgets import AdminJalaliDateWidget
 from datetime import datetime
 from .models import User
 from .models import Volunteer
@@ -29,7 +27,7 @@ class VolunteerRegisterForm(forms.ModelForm):
         fields = '__all__'
         widgets = {
             'gender': forms.Select(),
-            'birth': jDateInput(attrs={'widgets' : 'AdminjDateWidget'}),
+            'birth': JalaliDateField(attrs={'widgets' : 'AdminJalaliDateWidget'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'profile_pic': forms.FileInput(attrs={'class': 'form-control'}),
             'education': forms.Select(attrs={'class': 'text-end p-1 m-1'}),
