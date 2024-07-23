b3f82b5c82470e712d890799f12250a2b0029bb0
arta16h <sevda.hayati2015@gmail.com>
2024-07-23 17:43:30 +0330

    Chore: Slider admin register

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import  Volunteer, User
+from .models import  Volunteer, User, Slider
 
 # Register your models here.
 
 # admin.site.register(Job)
 admin.site.register(Volunteer)
 admin.site.register(User)
+admin.site.register(Slider)
