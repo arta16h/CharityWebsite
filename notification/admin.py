d75275871b14f0b72568cf3527019342b9c8db4b
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2024-07-30 05:04:42 +0330

    chore: registering notification models in admin panel

--- notification/admin.py
+++ notification/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Notification, NotificationCategory
 
 # Register your models here.
+admin.site.register(Notification)
+admin.site.register(NotificationCategory)
