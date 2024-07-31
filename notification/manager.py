8d03d81bd943d47f451d027e0ade08835ba0b6a8
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2024-07-31 21:19:46 +0330

    chore: updat3e notification manager to direct access to read notifications

--- notification/manager.py
+++ notification/manager.py
@@ -1,5 +1,9 @@
 from django.db import models
 
 class NotificationQuerySet(models.QuerySet):
+    
     def unread_notifications(self):
         return self.filter(is_seen=False)
+    
+    def read_notifications(self):
+        return self.filter(is_seen=True)
