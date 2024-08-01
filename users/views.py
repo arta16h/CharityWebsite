813b716e9bceeccf4bdfab631e36532b1a58d4c9
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2024-08-01 19:16:47 +0330

    style: removing redundant prints

--- users/views.py
+++ users/views.py
@@ -180,17 +180,14 @@ class LogoutView(View) :
 
 @login_required(login_url='login')
 def dashboard(request) :
-    print(request.FILES)
     user_update_form = CustomUserChangeForm(instance=request.user)
     if request.method == "POST":
-        print(f"{request.POST=}")
         user_update_form = CustomUserChangeForm(
             request.POST, 
             request.FILES, 
             instance=request.user,
         )
         if user_update_form.is_valid():
-            print("here")
             user_update_form.save()
             return redirect("dashboard")
         
