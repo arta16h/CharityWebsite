6befdceda58597f15d1c26467c4cbabf8c5989d2
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2024-08-02 22:37:13 +0330

    chore: donate view

--- payment/views.py
+++ payment/views.py
@@ -11,6 +11,10 @@ from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from notification.models import Notification, NotificationCategory
 from users.models import User
 from .models import Payment, PaymentMethod, PaymentCategory
+
+import json
+import uuid
+from datetime import datetime
 # Create your views here.
 
 
@@ -25,5 +29,24 @@ class DonateView(View):
         return render(request, 'donate.html', context)
     
     def post(self, request):
+        form_data = request.POST
+        if form_data.get("payment_method") == "cart_to_cart":
+            trace_id = uuid.uuid4()
+            if request.user.is_anonymous:
+                donates = json.loads(request.COOKIES.get("donates", "[]")).append({"payment_data": form_data, "payment_trace_id": trace_id})
+                request.COOKIES['donates'] = json.dumps(donates)
+            else:
+                Payment.objects.create(
+                    payment_trace_id=trace_id,
+                    request_user=request.user,
+                    payment_status=Payment.STATUS_DONE,
+                    payment_method=PaymentMethod.objects.get(name="cart_to_cart"),
+                    request_time=datetime.now(),
+                    total_amount=form_data.get("amount"),
+                    pay_id=Payment.generate_unique_payment_code(request.user.id, form_data.get("amount")),
+                    description=form_data.get("message")
+                )
+            context = {"payment_trace_id": trace_id}
+            return render(request, "payment/payment_confirm.html", context)
         messages.success(request, 'Thank you for your donation!')
         return redirect('donate')
