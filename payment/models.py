6407b51297198a7dea5fce38584f1e7624b5b567
arta16h <sevda.hayati2015@gmail.com>
2024-09-12 15:08:15 +0330

    Chore: renaming verbose name

--- payment/models.py
+++ payment/models.py
@@ -19,22 +19,22 @@ class PaymentCategory(models.Model):
 
     class Meta:
         db_table = 'payment_category'
-        verbose_name = _('دسته پرداخت')
-        verbose_name_plural = _("دسته های پرداخت")
+        verbose_name = _('کتگوری پرداخت')
+        verbose_name_plural = _("کتگوری های پرداخت")
 
     def __str__(self):
         return str(self.name)
 
 
 class PaymentMethod(models.Model):
-    name = models.CharField(_("name"), max_length=50)
-    slug = models.SlugField(_("slug"), null=False, blank=False)
-    description = models.CharField(_("description"), max_length=250, null=True, blank=True)
-    is_active = models.BooleanField(_("is active"), default=True)
-    image = models.ImageField(_("image"), upload_to='payment_method_images/', null=True, blank=True)
+    name = models.CharField(_("نام"), max_length=50)
+    slug = models.SlugField(_("اسلاگ"), null=False, blank=False)
+    description = models.CharField(_("توضیحات"), max_length=250, null=True, blank=True)
+    is_active = models.BooleanField(_("فعال"), default=True)
+    image = models.ImageField(_("تصویر"), upload_to='payment_method_images/', null=True, blank=True)
 
-    date_added = models.DateTimeField(_("date added"), auto_now_add=True)
-    date_modify = models.DateTimeField(_("date modify"), auto_now=True)
+    date_added = models.DateTimeField(_("زمان ثبت"), auto_now_add=True)
+    date_modify = models.DateTimeField(_("زمان ویرایش"), auto_now=True)
 
     class Meta:
         db_table = 'payment_method'
@@ -67,18 +67,18 @@ class Payment(models.Model):
         (STATUS_FAIL, "در پروسه انجام"),
     )
 
-    payment_trace_id = models.UUIDField(_("payment_id"), max_length=150, unique=True)
-    request_user = models.ForeignKey(User, verbose_name=_("request user"), on_delete=models.CASCADE)
-    payment_status = models.PositiveSmallIntegerField(_("payment_status"), choices=PAYMENT_STATUS_CHOICES)
-    payment_method = models.ForeignKey(PaymentMethod, verbose_name=_("payment method"), on_delete=models.CASCADE)
-    request_time = models.DateTimeField(_("request time"))
-    total_amount = models.PositiveBigIntegerField(_("total amount"))
+    payment_trace_id = models.UUIDField(_("payment trace id"), max_length=150, unique=True)
+    request_user = models.ForeignKey(User, verbose_name=_("کاربر"), on_delete=models.CASCADE)
+    payment_status = models.PositiveSmallIntegerField(_("وضعیت پرداخت"), choices=PAYMENT_STATUS_CHOICES)
+    payment_method = models.ForeignKey(PaymentMethod, verbose_name=_("روش پرداخت"), on_delete=models.CASCADE)
+    request_time = models.DateTimeField(_("زمان"))
+    total_amount = models.PositiveBigIntegerField(_("میزان پرداخت"))
     pay_id = models.CharField(_("pay id"), max_length=30, unique=True)
-    image = models.ImageField(_("image"), upload_to='payment_images/', null=True, blank=True)
-    description = models.TextField(_("description"), null=True, blank=True)
+    image = models.ImageField(_("تصویر"), upload_to='payment_images/', null=True, blank=True)
+    description = models.TextField(_("توضیحات"), null=True, blank=True)
 
-    date_added = models.DateTimeField(_("date added"), auto_now_add=True)
-    date_modify = models.DateTimeField(_("date modify"), auto_now=True)
+    date_added = models.DateTimeField(_("زمان ثبت"), auto_now_add=True)
+    date_modify = models.DateTimeField(_("زمان ویرایش"), auto_now=True)
 
     def __str__(self):
         return f'{self.payment_method} {self.payment_trace_id} {self.PAYMENT_STATUS_CHOICES_VIEW[self.payment_status]}'
@@ -91,3 +91,11 @@ class Payment(models.Model):
         db_table = 'payment'
         verbose_name = _('پرداخت')
         verbose_name_plural = _("پرداخت ها")
+
+class PaymentData(models.Model) :
+    card = models.BigIntegerField(verbose_name=_("شماره کارت"))
+
+    class Meta:
+        verbose_name = _('شماره کارت')
+        verbose_name_plural = _("شماره کارت ها")
+
