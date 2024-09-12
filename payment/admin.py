29f934502087a4ac1b26995d0c9484fc742d4d24
arta16h <sevda.hayati2015@gmail.com>
2024-09-12 15:08:42 +0330

    Chore: registering payment data

--- payment/admin.py
+++ payment/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Payment, PaymentMethod, PaymentCategory
+from .models import Payment, PaymentMethod, PaymentCategory, PaymentData
 
 # Register your models here.
 
 admin.site.register(Payment)
+admin.site.register(PaymentData)
 admin.site.register(PaymentMethod)
 admin.site.register(PaymentCategory)
