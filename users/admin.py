12df161d4430d778027b161d700476090a6ce1f4
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2025-03-14 17:06:35 +0330

    chore: user admin class

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import  Volunteer, User, Slider, Document, MainData
 
 # Register your models here.
 
 # admin.site.register(Job)
 admin.site.register(Volunteer)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Slider)
 admin.site.register(Document)
 admin.site.register(MainData)
