ffa7c6a681a1937d5c65be648204eefdab4b1525
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2025-03-14 17:43:03 +0330

    fix: custom user admin

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,33 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.admin import UserAdmin
 from .models import  Volunteer, User, Slider, Document, MainData
 
 # Register your models here.
 
+class CustomUserAdmin(UserAdmin):
+    
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+
 # admin.site.register(Job)
 admin.site.register(Volunteer)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Slider)
 admin.site.register(Document)
 admin.site.register(MainData)
