b182cbea05b207bd1c4de73fa42a1e7d8c61ea18
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2025-03-14 16:38:23 +0330

    chore: add notificatiojn sended users and seen by users inline

--- notification/admin.py
+++ notification/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 from .models import Notification, NotificationCategory
+from users.models import User
 
 # Register your models here.
-admin.site.register(Notification)
+
+class SendedUserInline(admin.TabularInline):
+    verbose_name = "sended to user"
+    model = Notification.users.through
+    extra = 0
+
+
+class SeenByUsersInline(admin.TabularInline):
+    verbose_name = "read by user"
+    model = Notification.is_seen_by.through
+    extra = 0
+
+
+class NotificationAdmin(admin.ModelAdmin):
+    model = Notification
+    inlines = [
+        SendedUserInline,
+        SeenByUsersInline
+    ]
+    exclude = ["users"]
+    readonly_fields = ("is_seen_by", )
+
+admin.site.register(Notification, NotificationAdmin)
 admin.site.register(NotificationCategory)
