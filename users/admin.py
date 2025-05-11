63a78803007db370fbc4327e17183d50db8a82ca
arta16h <sevda.hayati2015@gmail.com>
2025-05-11 18:34:05 +0330

    Chore: registering jadmin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.admin import UserAdmin
+import django.forms as forms
+import django_jalali.admin as jadmin
 from .models import  Volunteer, User, Slider, Document, MainData
 
 # Register your models here.
@@ -24,9 +26,22 @@ class CustomUserAdmin(UserAdmin):
         ),
         (_("Important dates"), {"fields": ("last_login",)}),
     )
+class VolunteerAdminForm(forms.ModelForm):
+    class Meta:
+        model = Volunteer
+        fields = '__all__'
+        widgets = {
+            'birth': jadmin.widgets.AdminjDateWidget,
+        }
 
+
+class VolunteerAdmin(jadmin.ModelAdmin):
+    form = VolunteerAdminForm
+    list_display = ('name', 'birth')
+    search_fields = ('name',)
+    list_filter = ('birth',)
 # admin.site.register(Job)
-admin.site.register(Volunteer)
+admin.site.register(Volunteer, VolunteerAdmin)
 admin.site.register(User, CustomUserAdmin)
 admin.site.register(Slider)
 admin.site.register(Document)
